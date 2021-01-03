dd7272e6cc48e84e5998eea6affcffde79b355b7
Jerrin Thomas <jd0christo1678@gmail.com>
2021-01-03 19:40:49 +0530

    Interface Segregation Peinciple

---
+++ SOLIDS/4 Interface Segregation Principle/Interface Segregation Principle Sol/Interface Segregation Principle/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace Interface_Segregation_Principle
+{
+    /* Interface Segregation Principle 
+     * Basic idea - If you have an initerface which includees too much stuff then beak it inot smaller interfaces
+     * 
+     * Here IMachine interface contains too many methods. [MultiFunctionPrinter inherit IMachine].
+     * IMachine can break into IPrinter, IScanner, and IFax interfaces.
+     * In this way, options can be provided to class, which interface that class want to inherit from.
+     * Here Printer class inherit only IPrinter whereas PhotCopier Class inherit IPrinter and IScanner
+     * Or new Interface can be made that inherit both IPinter and IScanner interfaces (interface can inherit interfaces).
+     * 
+     * for Example : IMultifunctionDevice interfaces
+     * 
+     */
+    public class Document
+    {
+    }
+
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Fax(Document d);
+        void Scan(Document d);
+    }
+
+    // ok if you need a multifunction machine
+    public class MultiFunctionPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Fax(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            //
+        }
+    }
+
+    public class OldFashionedPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Fax(Document d)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public interface IPrinter
+    {
+        void Print(Document d);
+    }
+
+    public interface IScanner
+    {
+        void Scan(Document d);
+    }
+
+    public class Printer : IPrinter
+    {
+        public void Print(Document d)
+        {
+
+        }
+    }
+
+    public class Photocopier : IPrinter, IScanner
+    {
+        public void Print(Document d)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public interface IMultiFunctionDevice : IPrinter, IScanner //
+    {
+
+    }
+
+    public struct MultiFunctionMachine : IMultiFunctionDevice
+    {
+        // compose this out of several modules
+        private IPrinter printer;
+        private IScanner scanner;
+
+        public MultiFunctionMachine(IPrinter printer, IScanner scanner)
+        {
+            if (printer == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(printer));
+            }
+            if (scanner == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(scanner));
+            }
+            this.printer = printer;
+            this.scanner = scanner;
+        }
+
+        public void Print(Document d)
+        {
+            printer.Print(d);
+        }
+
+        public void Scan(Document d)
+        {
+            scanner.Scan(d);
+        }
+    }
+}
